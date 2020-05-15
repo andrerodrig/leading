0b3d3c80a46f245e06c08cb53f18b0c82c8898ea
andrerodrig <andrelmarques11@gmail.com>
2020-05-15 04:04:59 -0300

    Add alerts to CRUD operations for the frontend

--- leadmanag/frontend/src/components/layout/Alerts.js
+++ leadmanag/frontend/src/components/layout/Alerts.js
@@ -1,9 +1,27 @@
 import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export class Alerts extends Component {
-  componentDidMount() {
-    this.props.alert.show("It works");
+  static propTypes = {
+    error: PropTypes.object.isRequired,
+    message: PropTypes.object.isRequired,
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error, alert, message } = this.props;
+    if (error !== prevProps.error) {
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+      if (error.msg.message)
+        alert.error(`Message: ${error.msg.message.join()}`);
+    }
+
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
+    }
   }
 
   render() {
@@ -11,4 +29,9 @@ export class Alerts extends Component {
   }
 }
 
-export default withAlert(Alerts);
+const mapStateToProps = (state) => ({
+  error: state.errors,
+  message: state.messages,
+});
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
